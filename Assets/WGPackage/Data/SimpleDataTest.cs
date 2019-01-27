1015a448bb17f7de9cec366c74edacf17851bfa6
WojciechGabel <wojciech.gabel@gmail.com>
2019-01-27 17:53:39 +0100

    add new settings

---
+++ Assets/WGPackage/Data/SimpleDataTest.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDataTest
+{
+    public string someText;
+    public List<SimpleDataTest> somecollection;
+}
--- Assets/WGPackage/IOC/IOC.cs
+++ Assets/WGPackage/IOC/IOC.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//https://www.codeproject.com/Articles/347651/Define-Your-Own-IoC-Container
 namespace WGPackage.IOC
 {
     public static class IOC
@@ -9,12 +10,23 @@ namespace WGPackage.IOC
         private static Dictionary<Type, Type> types = new Dictionary<Type, Type> ();
         private static Dictionary<Type, object> typeValues = new Dictionary<Type, object> ();
 
+        /// <summary>
+        /// Register interface to concrete type without instance(non-singleton)
+        /// </summary>
+        /// <typeparam name="TContract"></typeparam>
+        /// <typeparam name="TImplementation"></typeparam>
         public static void Register<TContract, TImplementation> ()
             where TImplementation : TContract
         {
             types[typeof ( TContract )] = typeof ( TImplementation );
         }
 
+        /// <summary>
+        /// Register type with an instance(singleton).
+        /// </summary>
+        /// <typeparam name="TContract"></typeparam>
+        /// <typeparam name="TImplementation"></typeparam>
+        /// <param name="value"></param>
         public static void Register<TContract, TImplementation> ( TImplementation value )
             where TImplementation : TContract
         {
